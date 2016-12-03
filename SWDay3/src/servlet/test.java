0b5647ef7b872ec67516e06029e31d90f99b7101
Wai Yan Oo <e0046649@u.nus.edu>
2016-12-03 14:15:23 +0800

    test

--- SWDay3/src/servlet/test.java
+++ SWDay3/src/servlet/test.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class test
  */
-@WebServlet("/test")
+@WebServlet("/combo")
 public class test extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
@@ -27,7 +27,7 @@ public class test extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		doProcess(request, response);
 	}
 
 	/**
@@ -38,7 +38,7 @@ public class test extends HttpServlet {
 		doProcess(request, response);
 	}
 	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		System.out.println("testig");
 	}
 
 }
